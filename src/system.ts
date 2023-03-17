14a0369e25e864504726442bb97da23e85fa4ed4
Wtko1 <60545167+Wtko1@users.noreply.github.com>
2023-03-17 16:33:39 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,7 +11,7 @@ export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.open
 
 - 由 [@wtko1](https://github.com/wtko1) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发，查看 [源码](https://github.com/wtko1/chatgpt-vercel)，欢迎自部署。
 
-- 该网站仅作为演示，不提供长期服务，网站密码为 \`hewentao\`，在左下角小齿轮填写密码，请勿滥用。
+- 该网站仅作为演示，不提供长期服务，网站密码为 \`何文涛\`，在左下角小齿轮填写密码，请勿滥用。
 
 - 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/wtko1/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
 
